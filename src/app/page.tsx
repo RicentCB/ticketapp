9bb02300f8d2c3532a17f8551a712c4ff12c12cf
“RicentCB“ <ricardo.cb009@gmail.com>
2024-10-23 11:14:34 -0600

    [UP] Formating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import Grid from "@mui/material/Grid2";
 import { useTheme } from "@mui/material/styles";
 import { useForm } from "react-hook-form";
-import { Box, Button, Container, CircularProgress, MenuItem, TextField, Typography, Select, FormControl, InputLabel, FormHelperText, } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  CircularProgress,
+  MenuItem,
+  TextField,
+  Typography,
+  Select,
+  FormControl,
+  InputLabel,
+  FormHelperText,
+} from "@mui/material";
 
 // Definir las opciones para el Select
-const units = ['Unidad 1', 'Unidad 2', 'Unidad 3'];
+const units = ["Unidad 1", "Unidad 2", "Unidad 3"];
 
 export default function NewTicketPage() {
   // Utilizar el hook useTheme para acceder al tema actual
@@ -19,7 +31,7 @@ export default function NewTicketPage() {
     formState: { errors },
   } = useForm();
   // Estado controlado para la unidad
-  const [selectedUnit, setSelectedUnit] = useState(''); // Valor inicial vacío
+  const [selectedUnit, setSelectedUnit] = useState(""); // Valor inicial vacío
   // Estado para el botón de "cargando"
   const [loading, setLoading] = useState(false);
 
@@ -27,37 +39,39 @@ export default function NewTicketPage() {
   const handleUnitChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setSelectedUnit(event.target.value as string);
   };
-  
+
   // Función que se ejecuta al enviar el formulario
   const onSubmit = async (data: any) => {
     setLoading(true);
     try {
-      const response = await fetch('/api/tickets', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+      const response = await fetch("/api/tickets", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data), // Envía los datos del formulario
       });
 
       if (response.ok) {
-        alert('Ticket creado exitosamente');
+        alert("Ticket creado exitosamente");
       } else {
-        alert('Error al crear el ticket');
+        alert("Error al crear el ticket");
       }
     } catch (error) {
-      console.error('Error al enviar el ticket:', error);
-      alert('Hubo un error al enviar los datos');
-    } finally{
+      console.error("Error al enviar el ticket:", error);
+      alert("Hubo un error al enviar los datos");
+    } finally {
       setLoading(false);
     }
   };
 
   return (
-    <Container maxWidth="sm"
-    sx={{
-      color: theme.palette.text.primary,
-      padding: theme.spacing(3), 
-      borderRadius: theme.shape.borderRadius,
-    }}>
+    <Container
+      maxWidth="sm"
+      sx={{
+        color: theme.palette.text.primary,
+        padding: theme.spacing(3),
+        borderRadius: theme.shape.borderRadius,
+      }}
+    >
       <Typography variant="h4" component="h1" gutterBottom>
         Ticket de asistencia Ingeniera
       </Typography>
@@ -67,18 +81,22 @@ export default function NewTicketPage() {
         noValidate
         autoComplete="off"
         onSubmit={handleSubmit(onSubmit)}
-        sx={{mt: 3}}
+        sx={{ mt: 3 }}
       >
         <Grid container spacing={2}>
           {/* Titulo */}
           <Grid size={12}>
-          <TextField
+            <TextField
               fullWidth
               label="Título*"
               variant="outlined"
-              {...register('title', { required: 'El título es requerido' })}
+              {...register("title", { required: "El título es requerido" })}
               error={!!errors.title} // Muestra error si hay
-              helperText={typeof errors.title?.message === 'string' ? errors.title.message : ''}
+              helperText={
+                typeof errors.title?.message === "string"
+                  ? errors.title.message
+                  : ""
+              }
             />
           </Grid>
 
@@ -88,20 +106,27 @@ export default function NewTicketPage() {
               fullWidth
               label="Solicitante*"
               variant="outlined"
-              {...register('creator', { required: 'El creador es requerido' })}
+              {...register("creator", { required: "El creador es requerido" })}
               error={!!errors.creator}
-              helperText={typeof errors.creator?.message === 'string' ? errors.creator.message : ''} // Verifica si el mensaje es una cadena
+              helperText={
+                typeof errors.creator?.message === "string"
+                  ? errors.creator.message
+                  : ""
+              } // Verifica si el mensaje es una cadena
             />
           </Grid>
 
           {/* Unidad */}
           <Grid size={12}>
-          <FormControl fullWidth error={!!errors.unit}>
+            <FormControl fullWidth error={!!errors.unit}>
               <InputLabel>Unidad*</InputLabel>
               <Select
                 value={selectedUnit}
                 label="Unidad*"
-                {...register('unit', { required: 'Selecciona una unidad', onChange:handleUnitChange })}
+                {...register("unit", {
+                  required: "Selecciona una unidad",
+                  onChange: handleUnitChange,
+                })}
               >
                 <MenuItem value="">
                   <em>Seleccione una unidad</em>
@@ -113,7 +138,9 @@ export default function NewTicketPage() {
                 ))}
               </Select>
               <FormHelperText>
-                {typeof errors.unit?.message === 'string' ? errors.unit.message : ''}
+                {typeof errors.unit?.message === "string"
+                  ? errors.unit.message
+                  : ""}
               </FormHelperText>
             </FormControl>
           </Grid>
@@ -126,9 +153,13 @@ export default function NewTicketPage() {
               variant="outlined"
               multiline
               rows={4}
-              {...register('description')}
+              {...register("description")}
               error={!!errors.description}
-              helperText={typeof errors.description?.message === 'string' ? errors.description.message : ''} // Verifica si el mensaje es una cadena
+              helperText={
+                typeof errors.description?.message === "string"
+                  ? errors.description.message
+                  : ""
+              } // Verifica si el mensaje es una cadena
             />
           </Grid>
 
@@ -142,7 +173,8 @@ export default function NewTicketPage() {
               disabled={loading} // Desactivar el botón mientras está "cargando"
               startIcon={loading ? <CircularProgress size={20} /> : null} // Mostrar el spinner mientras carga
             >
-              {loading ? 'Enviando...' : 'Crear Ticket'} {/* Cambia el texto del botón */}
+              {loading ? "Enviando..." : "Crear Ticket"}{" "}
+              {/* Cambia el texto del botón */}
             </Button>
           </Grid>
         </Grid>
