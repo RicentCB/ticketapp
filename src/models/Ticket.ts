a57eb7e107a909bcbbcaf2c40660e2d72b5bdb0b
“RicentCB“ <ricardo.cb009@gmail.com>
2024-10-23 11:38:44 -0600

    [FEAT] automatic date

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -7,15 +7,17 @@ export interface ITicket extends Document {
   creatorPhoneNumber: string;
   unit: string;
   description: string;
+  dateCreation: Date; 
 }
 
 // Definir el esquema de Mongoose
 const TicketSchema: Schema<ITicket> = new Schema({
   title: { type: String, required: true },
   creator: { type: String, required: true },
-  creatorPhoneNumber: { type: String, required: true },
+  creatorPhoneNumber: { type: String, required: false },
   unit: { type: String, required: true },
   description: { type: String, required: false },
+  dateCreation: { type: Date, default: Date.now },
 });
 
 // Exportar el modelo
