98ee7a46a082eb223dfe644315980d871b00743f
“RicentCB“ <ricardo.cb009@gmail.com>
2024-10-23 09:43:24 -0600

    [FEAT] Pagina de creacion de tickets

--- src/app/theme/index.ts
+++ src/app/theme/index.ts
@@ -4,6 +4,7 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
+    mode: 'light', // Establece el modo como 'light'
     primary: {
       main: '#1976d2',
     },
